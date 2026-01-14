20648b60742199f7c3a8af6ec35c32b9b0b085eb
Levyyyy <Levi.moreau@edu.sait.ca>
2026-01-14 16:23:30 -0700

    fixing duplicate vercel deployment

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          Homepage 6767676767
+          Homepage 676767676767
         </div>
       </main>
     </div>
