ffce8ed31272bcbe9c754851d9e1b57b500d6b8f
Levyyyy <Levi.moreau@edu.sait.ca>
2026-01-18 01:27:01 -0700

    Tackled most of homepage nav n footer

---
+++ src/app/components/Footer.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+
+export default function Footer() {
+
+  return (
+      <footer className="bg-[#31270C] w-full h-56 text-white items-center text-center justify-center flex">
+
+        <a href="https://www.facebook.com/p/Landscape-Craftsmen-61575247719417/">
+          <Image src="/icons/facebook.png" alt="Facebook icon" height={15} width={15} className="mr-10 scale-200"></Image>        
+        </a>
+
+
+        <div className="justify-center flex flex-col gap-2">
+          <p>LandscapeCraftsmen@gmail.com</p>
+          <p>(587)-438-6672</p>
+          <p>Calgary, AB, Canada</p>          
+        </div>
+
+        <a href="https://www.instagram.com/landscape.craftsmen">
+          <Image src="/icons/instagram.png" alt="Instagram icon" height={15} width={15} className="ml-10 scale-250"></Image>
+        </a>
+
+      </footer>
+
+  );
+}
+
+
