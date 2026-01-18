6844d774994d38a20f2f353bf302a90e26ed6869
Jearo <jiror6@gmail.com>
2026-01-17 22:28:13 -0700

    checking

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          Homepage 676767676767
+          Homepage 676767676767 67678676767667676767676767667676767676767676767676
         </div>
       </main>
     </div>
