4558369e820034bc3d9dd3018e9d43241262e41d
Levyyyy <Levi.moreau@edu.sait.ca>
2026-01-21 16:27:57 -0700

    merge resolve

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,19 @@ import Footer from "./components/Footer.js";
 
 export default function Home() {
   return (
-    <div className="overflow-hidden ">
-        <header className="flex min-h-screen w-full bg-white">
+    <div className="overflow-hidden">
+        <header className="flex w-full bg-white">
           <NavBar />         
         </header>
 
       <main className="flex min-h-screen w-full bg-white">
+        <div className="border-[#477a40] border-4 w-100 h-100 mt-10 ml-4">
+          <h2 className="text-3xl font-extrabold">The Contracting Company You Can Trust</h2>
+          <h3>All your landscaping needs done by professionals.</h3>
+          <p></p>
+        </div>
+
+
       </main>
 
       <Footer />
