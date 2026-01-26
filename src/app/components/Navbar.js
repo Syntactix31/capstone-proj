175c2ea3e29d07c6ad2465a94c7e8459574dec31
Levyyyy <Levi.moreau@edu.sait.ca>
2026-01-26 01:58:08 -0700

    navbar fixes

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -61,7 +61,8 @@ export default function NavBar () {
 
           </nav>
           
-          {navClicked && ( <DropDownMenu /> )}
+          {/* NOTE: Cant render here! */}
+          {/* {navClicked && ( <DropDownMenu /> )} */}
       </>
 
   );
