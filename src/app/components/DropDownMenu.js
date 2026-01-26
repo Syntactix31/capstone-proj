99b182779ae77c801d0cc05bc0d90580eaeb0d98
Levyyyy <Levi.moreau@edu.sait.ca>
2026-01-26 01:59:41 -0700

    shadow change for drop down

--- src/app/components/DropDownMenu.js
+++ src/app/components/DropDownMenu.js
@@ -4,7 +4,7 @@ export default function DropDownMenu () {
 
 
   return (
-      <div className="bg-[#477A40] w-80 h-100  flex flex-col gap-10 text-white text-center font-semibold p-10 *:hover:scale-105 *:transition-transform *:duration-200 *:active:opacity-50 *:active:scale-100 top-outline shadow-2xl z-100">
+      <div className="bg-[#477A40] w-80 h-100  flex flex-col gap-10 text-white text-center font-semibold p-10 *:hover:scale-105 *:transition-transform *:duration-200 *:active:opacity-50 *:active:scale-100 top-outline shadow z-100">
         <Link href="/projects">Projects</Link>
         <Link href="/quote">Get A Quote</Link>
         <Link href="/appointments">Book An Appointment</Link>
