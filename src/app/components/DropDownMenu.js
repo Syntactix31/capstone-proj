87c7217a0bd71661b137332b5842f7d4d795b973
Levyyyy <Levi.moreau@edu.sait.ca>
2026-02-01 01:46:02 -0700

    dropdown position fix

--- src/app/components/DropDownMenu.js
+++ src/app/components/DropDownMenu.js
@@ -4,7 +4,7 @@ export default function DropDownMenu ({ onClose }) {
 
 
   return (
-    <div className="absolute top-30 -right-4 z-[999] pointer-events-none">
+    <div className="absolute top-30 -right-0 z-[999] pointer-events-none">
       <div className="bg-[#477A40] w-80 h-100  flex flex-col gap-10 text-white text-center font-semibold p-10 *:hover:scale-105 *:transition-transform *:duration-200 *:active:opacity-50 pointer-events-auto *:active:scale-100 top-outline shadow-xl z-100">
         <Link href="/projects" onClick={onClose}>Projects</Link>
         <Link href="/quote" onClick={onClose}>Get A Quote</Link>
